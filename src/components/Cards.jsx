7766bc41c66627ec2f01a4e439c57730028ef408
enternal-L <88716148+enternal-L@users.noreply.github.com>
2023-12-24 17:50:40 -0500

    looking into external libraries for carousel

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,7 +10,6 @@ function Cards() {
     <div className='cards' id='projects'>
       <h1 className = 'cards__title'>Projects I worked on</h1>
       <div className='cards__container'>
-        <div className='cards__wrapper'>
           <ul className='cards__items'>
             <Card 
               title = 'MRover' 
@@ -19,31 +18,27 @@ function Cards() {
               path = 'https://github.com/umrover/mrover-ros/wiki'
               src = 'http://localhost:5173/mrover.jpg'
             />
-            <Card
+            <Card 
               title = 'MRover' 
               subtitle = 'Member' 
               desc = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam ipsam nobis, delectus adipisci dignissimos, ab obcaecati laborum non tenetur eveniet voluptatem laudantium itaque a error doloremque eum reiciendis minus temporibus.' 
               path = 'https://github.com/umrover/mrover-ros/wiki'
               src = 'http://localhost:5173/mrover.jpg'
             />
+            <Card 
+              title = 'MRover' 
+              subtitle = 'Member' 
+              desc = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam ipsam nobis, delectus adipisci dignissimos, ab obcaecati laborum non tenetur eveniet voluptatem laudantium itaque a error doloremque eum reiciendis minus temporibus.' 
+              path = 'https://github.com/umrover/mrover-ros/wiki'
+              src = 'http://localhost:5173/mrover.jpg'
+            />
+            {/* <CardItem
+              src=  {MRover}
+              text='MRover'
+              label='Robotics'
+              path='https://github.com/umrover/mrover-ros/wiki'
+            /> */}
           </ul>
-          <ul className="cards__items">
-            <Card
-              title = 'MRover' 
-              subtitle = 'Member' 
-              desc = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam ipsam nobis, delectus adipisci dignissimos, ab obcaecati laborum non tenetur eveniet voluptatem laudantium itaque a error doloremque eum reiciendis minus temporibus.' 
-              path = 'https://github.com/umrover/mrover-ros/wiki'
-              src = 'http://localhost:5173/mrover.jpg'
-            />
-            <Card
-              title = 'MRover' 
-              subtitle = 'Member' 
-              desc = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam ipsam nobis, delectus adipisci dignissimos, ab obcaecati laborum non tenetur eveniet voluptatem laudantium itaque a error doloremque eum reiciendis minus temporibus.' 
-              path = 'https://github.com/umrover/mrover-ros/wiki'
-              src = 'http://localhost:5173/mrover.jpg'
-            />
-          </ul>
-        </div>
       </div>
     </div>
   );
