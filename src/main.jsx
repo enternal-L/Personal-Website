46d71e970dd8f9f731cd4642bd3dfd3208ab68ed
enternal-L <88716148+enternal-L@users.noreply.github.com>
2023-12-25 22:04:14 -0500

    still fixing

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-
-// import BrowserRouter from 'react-router-dom/BrowserRouter'
-
-// ReactDOM.render((
-//    <BrowserRouter basename={process.env.PUBLIC_URL}>
-//      <App />
-//    </BrowserRouter>
-// ))
