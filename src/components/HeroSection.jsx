df04ba5b21aa037c66deeaf9a2e57250c7b317c6
enternal-L <88716148+enternal-L@users.noreply.github.com>
2023-12-25 23:07:45 -0500

    implementing lazy laoding into compilation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,6 +6,33 @@ import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 function HeroSection() {
 
+    document.addEventListener("DOMContentLoaded", function() {
+        var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+      
+        if ("IntersectionObserver" in window) {
+          var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(video) {
+              if (video.isIntersecting) {
+                for (var source in video.target.children) {
+                  var videoSource = video.target.children[source];
+                  if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                    videoSource.src = videoSource.dataset.src;
+                  }
+                }
+      
+                video.target.load();
+                video.target.classList.remove("lazy");
+                lazyVideoObserver.unobserve(video.target);
+              }
+            });
+          });
+      
+          lazyVideos.forEach(function(lazyVideo) {
+            lazyVideoObserver.observe(lazyVideo);
+          });
+        }
+    });
+
     const [TypeEffect] = useTypewriter({
         words: ['Computer Science Undergraduate'],
         cursor: true,
@@ -15,7 +42,7 @@ function HeroSection() {
     return (
         <>
             <div className = 'hero-container' id='top'>
-                <video src = {video} autoPlay loop muted></video>
+                <video className = 'lazy' src = {video} autoPlay loop muted></video>
                 <h1>Marvin Jirapongsuwan</h1>
                 <p><span>{TypeEffect}</span><Cursor cursorColor='white' /></p>
                 <div className = 'hero-btns'>
