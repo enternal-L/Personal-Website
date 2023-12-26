999b1bc84bc05ce0bc12950d18172d0218dc5ab6
enternal-L <88716148+enternal-L@users.noreply.github.com>
2023-12-25 23:19:32 -0500

    removed lazy loading, and added animation to compensate gray screen

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,34 +5,6 @@ import '../App.css'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 function HeroSection() {
-
-    document.addEventListener("DOMContentLoaded", function() {
-        var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
-      
-        if ("IntersectionObserver" in window) {
-          var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(video) {
-              if (video.isIntersecting) {
-                for (var source in video.target.children) {
-                  var videoSource = video.target.children[source];
-                  if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
-                    videoSource.src = videoSource.dataset.src;
-                  }
-                }
-      
-                video.target.load();
-                video.target.classList.remove("lazy");
-                lazyVideoObserver.unobserve(video.target);
-              }
-            });
-          });
-      
-          lazyVideos.forEach(function(lazyVideo) {
-            lazyVideoObserver.observe(lazyVideo);
-          });
-        }
-    });
-
     const [TypeEffect] = useTypewriter({
         words: ['Computer Science Undergraduate'],
         cursor: true,
