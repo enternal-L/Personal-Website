e8317b2259d864d642c52b0469f9024391382ead
enternal-L <88716148+enternal-L@users.noreply.github.com>
2024-01-06 05:09:21 -0500

    fixed contact me button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,17 @@
 import "./Button.css";
 import { Link } from 'react-router-dom';
-import Resume from '../files/Resume.pdf'
+import Resume from '../files/Resume.pdf';
 
 const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium' , 'btn--large'];
 
-let  source = ''
-let blank = ''
-
 export const Button = ({children, type, onClick, buttonStyle, buttonSize, download}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-    
-    if(download){
-        source = Resume
-        blank = '_blank'
-    }
 
     return(
-        <Link className = "btn-mobile" to = {source} target = {blank}>
+        <Link className = "btn-mobile" to = {download ? Resume : ''} target = {download ? '_blank' : ''}>
             <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`} onClick = {onClick} type = {type}>
                 {children}
             </button>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,11 +14,11 @@ function HeroSection() {
     return (
         <>
             <div className = 'hero-container' id='top'>
-                <video className = 'lazy' src = {video} autoPlay loop muted playsinline></video>
+                <video className = 'lazy' src = {video} autoPlay loop muted playsInline></video>
                 <h1>Marvin Jirapongsuwan</h1>
                 <p><span>{TypeEffect}</span><Cursor cursorColor='white' /></p>
                 <div className = 'hero-btns'>
-                    <Button className = 'btns' buttonStyle = 'btn--outline' buttonSize= 'btn--large' onClick={() => {
+                    <Button className = 'btns' buttonStyle = 'btn--outline' buttonSize= 'btn--large' download = {false} onClick={() => {
                         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
                     }}>Contact Me</Button>
                 </div>
